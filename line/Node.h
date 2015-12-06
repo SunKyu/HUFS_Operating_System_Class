c8dc242cca21ba171fd86a4478033a7a1cec8812
SunKyu <leesunkeuy@naver.com>
2015-12-06 20:52:04 +0900

    add Node struct and init function
    add Node struct and init function
    so robot can know each Node's adjancy Node's dir and id

---
+++ line/Node.h
@@ -0,0 +1,15 @@
+#ifndef _NODE_H_
+#define _NODE_H_
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
+struct Node{
+    int id;
+    int dir[4];//0, 1, 2, 3 -> up, down, left, right vertex
+}
+struct Node node_arr[14];
+(struct Node) * node_init();
+
+#endif
